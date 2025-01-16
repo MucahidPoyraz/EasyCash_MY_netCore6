0fd2141ab9e940587f8e4969fa924f88c4f9a769
mpoyraz <mchdardc@gmail.com>
2025-01-16 22:55:16 +0300

    ADDED : Entity Katmanı eklendi, Currency, CustomerAccount, CustomerAccountProcess entityleri eklendi, ef kütüphaneler ve identity kütüphaneleri eklendi

---
+++ Entity/Concrete/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using Entity.Abstract;
+
+namespace Entity.Concrete
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
