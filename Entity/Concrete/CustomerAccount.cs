187719e4df34044c5c98b1d613f1b2bb858b1d59
mpoyraz <mchdardc@gmail.com>
2025-01-17 21:39:45 +0300

    ADDED : dal katmanında generic repo lar eklendi validation ruller ve configurationlar eklendi, dto aktmanı eklendi, içerisi boş bırakıldı ihtiyaç üzerine eklenecek

--- Entity/Concrete/CustomerAccount.cs
+++ Entity/Concrete/CustomerAccount.cs
@@ -5,6 +5,8 @@
         public string? Number { get; set; }
         public decimal Balance { get; set; }
         public string? BankBranch { get; set; }
+        public Guid AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
         public Guid CurrencyId { get; set; }
         public Currency? Currency { get; set; }
     }
